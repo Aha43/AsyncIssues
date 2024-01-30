8daca648904a9973ca63b7f0a78942e2ac2ad995
Arne Halvorsen <arne.halvorsen@scaleaq.com>
2024-01-30 16:51:11 +0100

    started with some sample code

---
+++ AsyncIssues/CodeWithIssues.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncIssues
+{
+    public interface ICodeWithIssues
+    {
+        Task<string> DoSomethingThatGiveResultAsync();
+        Task DoSomethingWithNoResultAsync();
+    }
+
+    public class CodeWithIssuesNoSyncImpl : ICodeWithIssues
+    {
+        public Task<string> DoSomethingThatGiveResultAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DoSomethingWithNoResultAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
---
+++ AsyncIssues/DataContext.cs
@@ -0,0 +1,27 @@
+﻿namespace AsyncIssues
+{
+    public interface IDataContext
+    {
+        Task AddAsync(object data);
+        void Close();
+    }
+
+    public class DataContext : IDataContext
+    {
+        private bool _disposed = false;
+
+        public async Task AddAsync(object data)
+        {
+            await Task.Delay(1000);
+
+            if (_disposed)
+            {
+                throw new Exception("Can not save: Data context closed");
+            }
+        }
+
+        public void Close() =>  _disposed = true;
+
+    }
+
+}
