569fc63a83c374c82ecc7952d48a4374cffcdf9e
Arne Halvorsen <arne.halvorsen@scaleaq.com>
2024-02-02 15:26:56 +0100

    Renamed files to fit situation now using branches as example

--- AsyncIssues/SomeSytem.cs
+++ AsyncIssues/SomeSytem.cs
@@ -2,13 +2,13 @@
 {
     public interface ISomeSystem
     {
-        Task SaveState(DataContext context);
+        Task SaveStateAsync(DataContext context);
     }
     
 
     public class SomeSytem : ISomeSystem
     {
-        public async Task SaveState(DataContext context)
+        public async Task SaveStateAsync(DataContext context)
         {
             await Task.Delay(1000);
             context.Save(this);
--- CodeWithIssues/Program.cs
+++ CodeWithIssues/Program.cs
@@ -5,17 +5,17 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var theSystem = new SomeOtherSytem();
+        var theSystem = new SomeSytem();
 
-        UseOfSomeOtherSystem1(theSystem);
-        UseOfSomeOtherSystem2(theSystem);
-        UseOfSomeOtherSystem3(theSystem);
-        UseOfSomeOtherSystem4(theSystem);
+        UseOfSomeSystem1(theSystem);
+        UseOfSomeSystem2(theSystem);
+        UseOfSomeSystem3(theSystem);
+        UseOfSomeSystem4(theSystem);
 
         Thread.Sleep(3000);
     }
 
-    private static void UseOfSomeOtherSystem1(SomeOtherSytem system)
+    private static void UseOfSomeSystem1(SomeSytem system)
     {
         var ctx = new DataContext();
 
@@ -26,11 +26,11 @@ internal class Program
         // This is a case developer gets a warning by the 'standard .net' code analyzers because he or she 
         // calls an async method like a void method AND not store the returned Task (which would indicate
         // some strategy to handle awaiting effectively)
-        
+
         // Developer should ALSO known from the Async in the name...
     }
 
-    private static void UseOfSomeOtherSystem2(SomeOtherSytem system)
+    private static void UseOfSomeSystem2(SomeSytem system)
     {
         var ctx = new DataContext();
 
@@ -40,10 +40,11 @@ internal class Program
 
         // Reason is code analyzer observe developer take care of return value of a not awaited
         // async method which is a Task so assumed she / he know what she / he is doing. 
+
         // In this case the analyzers think to highly of developer... 
     }
 
-    private static void UseOfSomeOtherSystem3(SomeOtherSytem system)
+    private static void UseOfSomeSystem3(SomeSytem system)
     {
         var ctx = new DataContext();
 
@@ -53,22 +54,22 @@ internal class Program
 
         // Reason is code analyzer observe developer take care of return value of a not awaited
         // async method which is a Task so assumed she / he know what she / he is doing. 
-        // In this case the analyzers think to highly of developer...
+        // In this case the analyzer think to highly of developer...
         //
-        // Only difference between this case and UseOfSystem2 is that developer explicit ignore
-        // the Task, still reason to think developer know what hen is doing... Sadely...
+        // Only difference between this case and UseOfSystem2 is that developer explicit ignores
+        // the return method Task, still reason to think developer know what hen is doing... Sadely...
     }
 
-    private static void UseOfSomeOtherSystem4(ISomeOtherSystem system)
+    private static void UseOfSomeSystem4(ISomeSystem system)
     {
         var ctx = new DataContext();
 
         // Do the error of not awaiting AND NO warning!
-        system.SaveStateAsync(ctx); 
+        system.SaveStateAsync(ctx);
         ctx.Close();
 
         // This may be bit surprising and understandable reason for missing await:
-        // It is very similar to case 1 WHERE we get a warning.
+        // It is very similar to case 1 WHERE we get a warning BUT here we does not.
         //
         // Difference is we here are referencing an interface not an instance.
         // Interface methods that return Task does not require the implementation to be
